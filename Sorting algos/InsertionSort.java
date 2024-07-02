b2094d18f5c50004925bd7005c2f341582340c10
Damandeep <damandeep@shauryasoft>
2024-07-02 14:35:58 +0530

    Insertion Sort

--- Sorting algos/InsertionSort.java
+++ Sorting algos/InsertionSort.java
@@ -1,15 +1,12 @@
 public class InsertionSort {
     public static void main(String[] args) {
         System.out.println("Insertion Sort");
-        int [] arr={13,46,24,52,20,9};
-        for(int i=1;i<arr.length;i++){
+//        int [] arr={13,46,24,52,20,9};
+        int [] arr={
+            95 ,13 ,47 ,52 ,41 ,68 ,42
+        };
+        for(int i=1;i<=arr.length;i++){
             for(int j=i-1;j>0;j--){
-
-               if(arr[j]>arr[i]){
-                   int temp=arr[i];
-                   arr[i]=arr[j];
-                   arr[j]=temp;
-               }
 
                if(arr[j]<arr[j-1]){
                    int temp=arr[j];
