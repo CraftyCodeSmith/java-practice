e295b80eabfc2f685810e872cbcf5bfc6ece4856
Damandeep <damandeep@shauryasoft>
2024-07-02 12:21:53 +0530

    Selection sort

---
+++ Sorting algos/SelectionSort.java
@@ -0,0 +1,21 @@
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        System.out.println("Selection Sort");
+        int [] arr={13,46,24,52,20,9};
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+               if(arr[i]>arr[j])
+               {
+                   int temp=arr[i];
+                   arr[i]=arr[j];
+                   arr[j]=temp;
+               }
+            }
+        }
+        for(int ele:arr){
+            System.out.print(ele+" ");
+        }
+
+    }
+}
